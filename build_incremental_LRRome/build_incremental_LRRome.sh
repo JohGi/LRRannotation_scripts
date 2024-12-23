875e1dbf295cfc0d24221a6191c1f05d9053146f
Johanna Girodolle <38213007+JohGi@users.noreply.github.com>
2024-12-23 13:42:56 +0100

    Fixed a file name

--- build_incremental_LRRome/build_incremental_LRRome.sh
+++ build_incremental_LRRome/build_incremental_LRRome.sh
@@ -18,7 +18,7 @@ gff_list=/home/girodollej/scratch/2024_LRR/02_results/06_LRRTRANSFER_LAUNCH_PREP
 initial_LRRome=/storage/replicated/cirad/projects/GE2POP/2023_LRR/IRGSP/LRRome
 initial_LRR_gff=/storage/replicated/cirad/projects/GE2POP/2023_LRR/IRGSP/Oryza_Nipponbare_IRGSP-1.0_LRR-CR__20220209.gff
 exp_ref_folder=/storage/replicated/cirad/projects/GE2POP/2023_LRR/SVEVO3/Data_Package_01_12_22
-exp_prefix=SVEVO_July
+exp_prefix=DWSvevo3December
 init_prefix=IRGSP
 
 ## ------------------------------- FUNCTIONS --------------------------------------------- ##
@@ -195,7 +195,7 @@ merge_LRRome $initial_LRRome $exp_LRRome 03_LRRome
 
 concat_gff $init_prefix $exp_prefix $initial_LRR_gff
 
-exp_LRR_gff=$(realpath 02_build_exp_LRRome/LRR_ANNOT/SVEVO_July_LRR.gff)
+exp_LRR_gff=$(realpath 02_build_exp_LRRome/LRR_ANNOT/${exp_prefix}_LRR.gff)
 create_info_locus $exp_LRR_gff $initial_LRR_gff $exp_prefix $init_prefix
 
 write_infos $exp_prefix
