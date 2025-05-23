eda5ccc4ba10247a4de9eef9afc8e1cf335f28fc
JohGi <tjohanna.girodolle@gmail.com>
2025-05-23 15:00:19 +0200

    add more info in histogram title

--- CDScompR_utils/src/CDScompR_lib/plots.py
+++ CDScompR_utils/src/CDScompR_lib/plots.py
@@ -1,4 +1,3 @@
-import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
@@ -28,20 +27,38 @@ def plot_identity_hist_from_csv(csv, ref_name, alt_name, out_path):
 
     # Convertir score en numérique
     values = pd.to_numeric(filtered_df[score_col], errors="coerce").dropna()
-    count = values.shape[0]
+    nb_common_genes = values.shape[0]
+    nb_genes_ref_only = ref_gff_nb_genes - nb_common_genes
+    nb_genes_alt_only = alt_gff_nb_genes - nb_common_genes
 
     # Print info
     print(f"\nComparison of {ref_name} with {alt_name}:")
-    print(f"--- Common genes: {count}")
-    print(f"--- Genes only in {ref_name}: {ref_gff_nb_genes - count}")
-    print(f"--- Genes only in {alt_name}: {alt_gff_nb_genes - count}")
+    print(f"--- Common genes: {nb_common_genes}")
+    print(f"--- Genes only in {ref_name}: {nb_genes_ref_only}")
+    print(f"--- Genes only in {alt_name}: {nb_genes_alt_only}")
 
     # Tracer
+
     plt.figure()
     plt.hist(values, bins=30, edgecolor="black")
-    plt.title(
-        f"{ref_name} vs. {alt_name}:\nID scores of their {count} overlapping genes"
+
+    plt.text(
+        0.5,
+        1.1,
+        f"{ref_name} vs. {alt_name}:\nID scores of their {nb_common_genes} overlapping genes",
+        fontsize=12,
+        ha="center",
+        transform=plt.gca().transAxes,
     )
+    plt.text(
+        0.5,
+        1.02,
+        f"[{ref_name}\u2192( {nb_genes_ref_only} ( {nb_common_genes} ) {nb_genes_alt_only} )\u2190{alt_name}]",
+        fontsize=10,
+        ha="center",
+        transform=plt.gca().transAxes,
+    )
+
     plt.xlabel("Identity Score (%)")
     plt.ylabel("Fréquence")
     plt.xlim(-5, 105)
