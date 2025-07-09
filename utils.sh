8235d02957657dc332005187ac4239a4724dc701
JohGi <tjohanna.girodolle@gmail.com>
2025-07-09 19:41:10 +0200

    fix: get_gene_class now works even if the class is the first attribute after comment=

--- utils.sh
+++ utils.sh
@@ -10,3 +10,24 @@ function create_fake_infoLocus() {
       print Gene_ID, "UC", "Canonical"
     }}' $gff >$out_file
 }
+
+get_gene_class() {
+  local gff=$1
+  awk -F '[ \t;]' 'BEGIN{OFS="\t"}{if ($3 == "gene"){
+    gsub(/comment=/, "", $0)
+    ID = ""
+    Class = ""
+    for (i = 1; i <= NF; i++) {
+      if ($i ~ /^ID=/) {
+        ID=gensub("ID=", "", "g", $i)
+      }
+      if ($i ~ /^Class=/) {
+        Class=gensub("Class=", "", "g", $i)
+      }
+      if ($i ~ /^Gene-Class:/) {
+        Class=gensub("Gene-Class:", "", "g", $i)
+      }
+    }
+    print ID, Class
+  }}' $gff
+}
